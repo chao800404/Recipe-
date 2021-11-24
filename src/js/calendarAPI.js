6cf9c0c82fd6e1943349ac94aa815acaeb0ae272
Chao <a0921342997@gmail.ocm>
2021-11-25 05:55:17 +0800

    upadate key

--- src/js/calendarAPI.js
+++ src/js/calendarAPI.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID =
-  '506620073844-f6ihqeu13if685pm20gid9jatgru37o4.apps.googleusercontent.com';
+  '506620073844-0mmvs8p8b8mlne1vuq8b5m4pqjmp2hfc.apps.googleusercontent.com';
 export const API_KEY = 'AIzaSyBKukpVixz43PBXFVzqncS1bGiW0uL1OcE';
 export const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
