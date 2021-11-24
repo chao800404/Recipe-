6d6b255287d1c3220015301e425cac8c197e7afc
Chao <a0921342997@gmail.ocm>
2021-11-25 06:13:59 +0800

    new final

--- src/js/calendarAPI.js
+++ src/js/calendarAPI.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID =
-  '506620073844-0mmvs8p8b8mlne1vuq8b5m4pqjmp2hfc.apps.googleusercontent.com';
+  '506620073844-v26otru3li1ngur23vejpobbmmjf4t1f.apps.googleusercontent.com';
 export const API_KEY = 'AIzaSyBKukpVixz43PBXFVzqncS1bGiW0uL1OcE';
 export const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
